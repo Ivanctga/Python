8eae6166a1a5da98c623036e059d430e9b32609c
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-01-21 09:40:08 -0300

    salve29-1

--- curso_python/segunda/aula29.py
+++ curso_python/segunda/aula29.py
@@ -4,6 +4,9 @@ try -> tentar executar o código
 except -> ocorreu algum erro ao tentar executar
 """
 
-print(1234)
-print(456)
-int('a')
+numero_str = input('Vou dobrar o número que vc digitar: ')
+
+print(numero_str.isdigit())
+
+numero_float = float(numero_str)
+print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
