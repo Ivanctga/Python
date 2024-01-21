c8f8ede2bdb4c8a3a6624a4d6a8c6c6d3016bbc0
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-01-21 10:51:37 -0300

    salve32

--- curso_python/segunda/aula29.py
+++ curso_python/segunda/aula29.py
@@ -6,7 +6,17 @@ except -> ocorreu algum erro ao tentar executar
 
 numero_str = input('Vou dobrar o número que vc digitar: ')
 
-print(numero_str.isdigit())
+try:
+    print('STR:', numero_str)
+    numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
+    print(f' O dobro de  {numero_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número')
 
-numero_float = float(numero_str)
-print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+
+# if numero_str.isdigit():
+#.....numero_float = float(numero_str)
+#..... print(f' O dobro de  {numero_str} é {numero_float * 2:.2f}')
+#else
+#....print('Isso não é um número) 
---
+++ curso_python/segunda/aula32.py
@@ -0,0 +1,6 @@
+"""
+Faça um programa que peça ai usúario paa digitar um número,
+informe se este número é par ou ímpar. Caso o usúario não digitar
+um número inteiro, informe que não é um número inteiro.
+"""
+
