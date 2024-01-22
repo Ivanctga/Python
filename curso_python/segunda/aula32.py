cb9ebf6656bad5a098c29c1bebb742accbff7a95
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-01-22 07:28:06 -0300

    salve32-1

--- curso_python/segunda/aula32.py
+++ curso_python/segunda/aula32.py
@@ -1,6 +1,36 @@
 """
-Faça um programa que peça ai usúario paa digitar um número,
+Faça um programa que peça ou usúario paa digitar um número,
 informe se este número é par ou ímpar. Caso o usúario não digitar
 um número inteiro, informe que não é um número inteiro.
 """
 
+# entrada = input('Digite um número: ')
+
+# if entrada.isdigit():
+#     entrada_int = int(entrada)
+#     par_impar = entrada_int % 2 == 0
+#     par_impar_texto = 'impar'
+    
+#     if par_impar:
+#         par_impar_texto = "par"
+
+#     print(f'O número {entrada_int} é {par_impar_texto}')
+
+# else:
+#     print('Você não digitou um número inteiro')
+
+
+entrada = input('Digite um número: ')
+
+try:
+    entrada_int = float(entrada)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'impar'
+    
+    if par_impar:
+        par_impar_texto = "par"
+
+    print(f'O número {entrada_int} é {par_impar_texto}')
+
+except:
+    print('Você não digitou um número inteiro')
