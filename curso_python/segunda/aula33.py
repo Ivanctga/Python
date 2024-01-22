276022ddec45699336aac34b9392e14ff653071c
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-01-22 14:17:07 -0300

    salve33-5

---
+++ curso_python/segunda/aula33.py
@@ -0,0 +1,10 @@
+"""
+Imutáveis que vimos: str, int, float, bool
+"""
+
+string = 'ivan Lopes'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(outra_variavel)
+print(string.capitalize())
+print(string.zfill(100))
---
+++ curso_python/segunda/aula34.py
@@ -0,0 +1,11 @@
+"""
+Repetições
+Executa uma ação enquando uma condição for verdadeira
+"""
+
+condicao = True
+
+while condicao:
+    print(1)
+    print(2)
+    print(3)
