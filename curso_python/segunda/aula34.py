b36810f0433e275359c293f8c6356b278df2169b
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-01-22 14:31:56 -0300

    aula34

--- curso_python/segunda/aula34.py
+++ curso_python/segunda/aula34.py
@@ -6,6 +6,10 @@ Executa uma ação enquando uma condição for verdadeira
 condicao = True
 
 while condicao:
-    print(1)
-    print(2)
-    print(3)
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+
+print('Acabou')
