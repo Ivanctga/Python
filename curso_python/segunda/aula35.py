0a5309a180c44e2bceaaaf1eb479c9eee73c0764
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-01-24 07:42:53 -0300

    salve36

---
+++ curso_python/segunda/aula35.py
@@ -0,0 +1,14 @@
+"""
+Repetições 
+whilw (enquanto)
+Executa um ação enquanto uma condição for verdaderia
+Loop infinito -> Quando um código não tem fim
+"""
+contador = 0
+
+while contador < 10:
+    contador = contador + 1
+    print(contador)
+    ...
+
+print('Acabou')
