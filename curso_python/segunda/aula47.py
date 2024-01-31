a8f9b1c70ecd3fc4461f04af7f03c754847d1daa
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-01-31 05:17:03 -0300

    salve48

--- curso_python/segunda/aula47.py
+++ curso_python/segunda/aula47.py
@@ -1,8 +1,13 @@
+import os
+
 palavra_secreta = 'perfume'
 letras_acertadas = ''
+numero_tentativas = 0
 
-while True:
+while True:  
+
     letra_digitada = input('Digite uma letra: ')
+    numero_tentativas += 1
 
     if len(letra_digitada) > 1:
         print('Digite apenas uma letra.')
@@ -11,10 +16,21 @@ while True:
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
 
+    palavra_formada = ''
     for letra_secreta in palavra_secreta:
         if letra_secreta in letras_acertadas:
-            print(letras_acertadas)
+            palavra_formada += letra_secreta
+            
         else:
-            print('*')
+            palavra_formada += '*'
+    
+    print('Palavra formada:', palavra_formada)
+
+    if palavra_formada == palavra_secreta:        
+        print('Você ganhou! parabéns"')
+        print('A palavra era,',palavra_secreta)
+        print('Tentativas:', numero_tentativas)
+        letras_acertadas = ''
+        numero_tentativas = 0
 
     
