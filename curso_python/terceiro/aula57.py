63c2e6eb806f7a1104e1705de69a427390da39ad
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-02-02 15:13:26 -0300

    salve58

--- curso_python/terceiro/aula57.py
+++ curso_python/terceiro/aula57.py
@@ -10,4 +10,12 @@ salas = [
     ['Luiz', 'João', 'Eduardo', ], # 2
 ]
 
-print(salas)
+# print(salas[1][0])
+# print(salas[0][1])
+# print(salas[2][2])
+# print(salas[2][3][3])
+
+for sala in salas:
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
