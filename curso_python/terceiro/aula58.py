4b75a96976989de66c97e9a9a7c79120be406b81
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-02-02 16:06:07 -0300

    salve59

---
+++ curso_python/terceiro/aula58.py
@@ -0,0 +1,9 @@
+# Desempacotamento em chamadas
+# de métodos e funções
+
+string = 'ABC'
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduardo']
+tupla = 'Python', 'e', 'legal'
+
+for nome in lista:
+    print(nome)
