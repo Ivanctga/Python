f3c2fd6c89148d874b447777b71c01af38595080
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-02-02 07:41:19 -0300

    salve

--- curso_python/terceiro/aula56.py
+++ curso_python/terceiro/aula56.py
@@ -5,3 +5,9 @@ join - une uma string
 """
 
 frase = 'Olha só que, coisa interessante'
+lista_frases = frase.split()
+
+for i, frase in enumerate(lista_frases):
+    lista_frases[i] = lista_frases[i].strip()
+
+print(lista_frases)
---
+++ curso_python/terceiro/aula57.py
@@ -0,0 +1,13 @@
+"""
+Lista de listas e seus índices
+"""
+salas = [
+    #0.........2
+    ['Maria', 'Helena', ], # 0
+    # 0
+    ['Elaine', ],  # 1
+    # 0.......1.......2
+    ['Luiz', 'João', 'Eduardo', ], # 2
+]
+
+print(salas)
