4184a86f3a60bfc0733938d294e2cd37b65fd75b
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-02-03 06:21:53 -0300

    salve60.1

--- curso_python/terceiro/aula60.py
+++ curso_python/terceiro/aula60.py
@@ -1,7 +1,37 @@
-cpf = '74682489070'
-nove_digitos = cpf[:9]
 
-contador_regressivo = 10
+cpf_enviado = '74682489070'
+nove_digitos = cpf_enviado[:9]
+
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
 
 for digito in nove_digitos:
-    print(digito)
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+
+digito_1 = (resultado_digito_1 * 10 ) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digito = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0 
+
+for digito in dez_digito:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+novo_cpf = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_enviado == novo_cpf:
+    print(f'{cpf_enviado} é valido')
+else:
+    print('CPF inválido')
+
+
+
+
